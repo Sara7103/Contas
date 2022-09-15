1dfffbad1c9c851161bae56dc6237e51d5cc073b
Sara7103 <sarapereiraalcantara123@gmail.com>
2022-09-14 22:18:10 -0300

    first commit

---
+++ conta_comum.php
@@ -0,0 +1,45 @@
+<?php 
+
+    Class Conta{
+        private $nro_conta;
+        private $dt_abertura;
+        private $dt_encerramento;
+        private $situacao;
+        private $senha;
+        private $saldo;
+
+        function abrir_conta(){
+
+        }
+
+        function consultar_conta(){
+
+        }
+
+        function validar_senha(){
+
+        }
+
+        function saldo_conta(){
+
+        }
+
+        function extrato_conta(){
+
+        }
+
+        function sacar_valor(){
+
+        }
+    
+        function depositar_valor(){
+
+        }
+
+        function encerrar_conta(){
+            
+        }
+
+        
+    }    
+?>
---
+++ conta_especial.php
@@ -0,0 +1,13 @@
+<?php
+
+    include_once 'conta_comum.php';
+
+    class Especial extends Conta{
+        function __construct($nro_conta,$dt_abertura,$dt_encerramento,
+        $situacao,$senha,$saldo){
+            
+
+        }
+    }
+
+?>
